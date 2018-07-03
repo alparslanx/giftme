a3a86a1c8f59572c8e232aaacae2468c5303a038
Alparslan <alparslan@acunn.com>
2018-07-04 00:24:09 +0300

    Most Popular Last Week 60 minute cache added.

--- app/Http/Controllers/Api/User/User/MostPopularController.php
+++ app/Http/Controllers/Api/User/User/MostPopularController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\User\User;
 
 use App\UserGift;
+use Illuminate\Filesystem\Cache;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
@@ -11,6 +12,11 @@ class MostPopularController extends Controller
 {
     public function last_week()
     {
-        return UserGift::join('users','users.id','=','user_gifts.user_id')->select('users.name',DB::raw('count(1)*10 AS score'))->where('user_gifts.status',1)->get()->toJson();
+        $lastWeekCache = Cache::get('most_popular_last_week');
+        if(!$lastWeekCache){
+            $lastWeekCache = UserGift::join('users','users.id','=','user_gifts.user_id')->select('users.name',DB::raw('count(1)*10 AS score'))->where('user_gifts.status',1)->get()->toJson();
+            Cache::put('most_popular_last_week', $lastWeekCache, 60);
+        }
+        return $lastWeekCache;
     }
 }
